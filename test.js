4369c688d9e348e9a9723d85e40968426129aacc
Ally <49165015+allyssapascual@users.noreply.github.com>
2020-03-14 17:13:20 +0000

    update checkUser test

--- test.js
+++ test.js
@@ -1,31 +1,16 @@
 QUnit.test(
-  "Test the login page: takes two inputs (username and password) and checks if username is registered user",
+  "Test the login page: checks if username is registered",
 
   function (assert) {
-
     assert.ok(
       typeof checkUser === "function",
       "Create a `checkUser` function."
     );
 
-    checkUser("up121212");
-
-    const done = assert.async(1);
-    assert.expect(2);
-
-    setTimeout(checkUser2, 500);
-    setTimeout(checkUser3, 1000);
-
-    function checkUser2() {
-      assert.ok(checkUser("up111111"));
-    };
-
-    function checkUser3() {
-      assert.ok(checkUser("up123456"));
-    };
+    assert.ok(checkUser("up121212"));
+    assert.ok(checkUser("up123456"));
+    assert.ok(checkUser("up111111"));
 
     done();
   }
-
-
 )
