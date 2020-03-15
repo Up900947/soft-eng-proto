6510c85b55bcba7af7204b2f77c97948e2501b4e
Ally <49165015+allyssapascual@users.noreply.github.com>
2020-03-15 08:15:42 +0000

    test checkPassword
    login page function checkPassword

--- test/source.js
+++ test/source.js
@@ -1,3 +1,4 @@
+//validation for user
 function checkUser(users, test_username) {
   const username = test_username;
   let found = false;
@@ -8,4 +9,15 @@ function checkUser(users, test_username) {
     }
   }
   return found;
+}
+
+//validation for password
+function checkPassword(user, test_username) {
+    const password = test_username;
+
+    if (user === password) {
+      return true;
+    } else {
+      return false;
+    }
 }
--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-
 QUnit.test(
   "Test the login page: checks if username is registered",
 
@@ -18,6 +17,45 @@ QUnit.test(
     assert.notOk(
       checkUser(["up121212", "up111111", "up123456"], "up123123"),
       "Check invalid values"
+    );
+  }
+)
+
+QUnit.test(
+  "Test the login page: checks if password is valid and registered",
+
+  function (assert) {
+    assert.ok(
+      typeof checkPassword === "function",
+      "Create a `checkPassword` function."
+    );
+
+    // registered/valid values
+    assert.ok(
+      checkPassword("123", "123"),
+      "Check up123456 value (valid)"
+    );
+    assert.ok(
+      checkPassword("111", "111"),
+      "Check up111111 value (valid)"
+    );
+    assert.ok(
+      checkPassword("1212", "1212"),
+      "Check up121212 value (valid)"
+    );
+
+    //expect fail
+    assert.notOk(
+      checkPassword("123", "1234"),
+      "Check up123456 value (invalid)"
+    );
+    assert.notOk(
+      checkPassword("111", "1114"),
+      "Check up111111 value (invalid)"
+    );
+    assert.notOk(
+      checkPassword("1212", "12124"),
+      "Check up121212 value (invalid)"
     );
   }
 )
