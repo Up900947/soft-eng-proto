d27b272cfbed3149e6c0bfbaa66dc397b0f3117d
Up900947 <56257516+Up900947@users.noreply.github.com>
2020-03-26 17:20:19 +0000

    modules added

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://up900947:<password>@cluster0-s1fdm.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
--- server.js
+++ server.js
@@ -5,8 +5,64 @@ const multer = require('multer');
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
 
 const uploader = multer({
   dest: 'upload',
@@ -92,4 +148,6 @@ app.post('/api/users', express.json(), postUser);
 app.post('/api/files', uploader.single('file'), express.json(), asyncWrap(uploadFile));
 app.get('/api/files:id', asyncWrap(getFiles));
 
-app.listen(8080);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
